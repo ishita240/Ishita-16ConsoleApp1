0acd633805b1a25b4bd0385ec51e91897250e716
A <A@ANUJ>
2023-09-30 21:48:31 -0400

    Add project files.

---
+++ Ishita 16ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ishita_16ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tmp;
+            Console.Write("\n\n");
+            Console.Write("Accept a temperature in centigrade and display a suitable message:\n");
+            Console.Write("--------------------------------------------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input days temperature : ");
+            tmp = Convert.ToInt32(Console.ReadLine());
+            if (tmp < 0)
+                Console.Write("Freezing weather.\n");
+            else if (tmp < 10)
+                Console.Write("Very cold weather.\n");
+            else if (tmp < 20)
+                Console.Write("Cold weather.\n");
+            else if (tmp < 30)
+                Console.Write("Normal in temp.\n");
+            else if (tmp < 40)
+                Console.Write("Its Hot.\n");
+            else
+                Console.Write("Its very hot.\n");
+        }
+    }
+}
